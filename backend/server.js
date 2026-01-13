1c2bf2377ad308f5ab7994ecdb99e3eb7ae10aab
Omee-Yehme <omingale786@gmail.com>
2026-01-13 14:34:28 +0530

    Minor Fix

--- backend/server.js
+++ backend/server.js
@@ -12,29 +12,13 @@ const baseRoutes = require("./src/routes/baseRoutes");
 const dashboardRoutes = require("./src/routes/dashboardRoutes");
 const app = express();
 
-const allowedOrigins = [
-    "http://localhost:5173",
-    "https://military-asset-management-system-rzk5.onrender.com"
-];
-
 app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error("Not allowed by CORS"));
-        }
-    },
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"]
+    origin: "http://localhost:5173",
+    credentials: true
 }));
 
-app.options("*", cors());
-
 app.use(express.json());
- 
+
 
 mongoose.connect(process.env.MONGODB_URI)
     .then(() => console.log('MongoDB connected'))
