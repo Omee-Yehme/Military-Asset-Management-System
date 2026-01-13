10f4f81879ae18034a2ef2f0a7de4f3838b5b59e
Omee-Yehme <omingale786@gmail.com>
2026-01-13 16:26:09 +0530

    Project Ready

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ const app = express();
 /* -------------------- Middleware -------------------- */
 
 app.use(cors({
-    origin: ["http://localhost:5173", "https://military-asset-management-system-rzk5.onrender.com"],
+    origin: ["https://mams-eta.vercel.app/", "https://military-asset-management-system-rzk5.onrender.com"],
     credentials: true
 }));
 
